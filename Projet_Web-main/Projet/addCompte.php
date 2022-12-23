6dfe118e5d5f4606acbc1421ebf0b46ddbd24abf
zertyfaucher <wendlingqn@gmail.com>
2022-12-23 22:19:29 +0100

    addCompte.php normalement finit

--- Projet_Web-main/Projet/addCompte.php
+++ Projet_Web-main/Projet/addCompte.php
@@ -5,33 +5,21 @@ function addCompte($nom,$prenom,$mail,$mdp) {
         // se connecter à la base de données
         $pdo = seConnecterBD();
         // préparer (compiler => générer code exécutable) la requête
-        $sql="SELECT * FROM utilisateur where login =:vallogin and mdp =:valmdp";// a modif par le insert et faire verif avant
+        $sql="INSERT INTO clients (Nom, Prenom, AdresseMail, mdp) VALUES (:valnom,:valprenom,:valmail,:valmdp)";
         $stmt = $pdo->prepare($sql);
         // initaliser la valeur des paramètres de la requête (avant son exécution)
-        $stmt->bindParam(":vallogin", $login);
-        $stmt->bindParam(":valmdp", $mdp);
+        $stmt->bindParam(":valnom", $nom);
+        $stmt->bindParam(":valprenom", $prenom);
+        $stmt->bindParam(":valmail", $mail);
+        $stmt->bindParam(":valmail", $mdp);
         // exécuter la requête (par le au SGBD)
-        $bool = $stmt->execute();
-        // récuéprer le résultat de la requête dans un tabeau associatif
-        $resultats = $stmt->fetchAll(PDO::FETCH_ASSOC);
-        // calculer le résultat de fonction (compte) à partir du résultat de la requête
-        $compte = null;
-        if (count ($resultats) > 0)
-            $compte = $resultats [0];
-        /* for ($resultats as $uneLigne){
-            ...
-        }
-        */
-        // fermer le curseur (libérer de la mémoire)
+        $stmt->execute();
         $stmt->closeCursor();
-        // retourner la réponse calculée (compte)
-
-        return $compte;
     }
     catch (PDOException $e) {
         // Erreur à l'exécution de la requête
         $erreur = $e->getMessage();
-        echo utf8_encode("Erreur d'accès à la base de données : $erreur \n");
+        echo utf8_encode("Erreur d'accès à la base de données ou creation de compte: $erreur \n");
         return null;
     }
 }
